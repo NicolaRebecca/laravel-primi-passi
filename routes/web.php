b148b4b0c0ba722d4b9122ec93cc9741c66dd785
NicolaRebecca <master9895@gmail.com>
2021-09-01 14:52:06 +0200

    ex. + bonus

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',
+    [
+        'name' => 'Stranger'
+    ]
+);
+});
+
+Route::get('/page2', function () {
+    return view('page2');
+});
+
+Route::get('/page3', function () {
+    return view('page3');
 });
